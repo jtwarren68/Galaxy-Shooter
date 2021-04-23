afa4d06de9d77aae09804f700d17262432d39b36
jtwarren68 <81195744+jtwarren68@users.noreply.github.com>
2021-04-23 17:26:44 -0500

    Simple Player Movment with Contraints

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    //private Transform _myTransform;
+
+    void Start()
+    {
+        // take the current position and assign start position to (o,0,0)
+        transform.position = Vector3.zero;
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0f) * (_speed * Time.deltaTime));
+        
+        // if y = 0 stop movement
+        // if x = 11.7 then x = -11.7 and vice versa
+
+        if (transform.position.y >= 3f)
+        {
+            transform.position = new Vector3(transform.position.x, 3f, 0f);
+        }
+
+        if (transform.position.y <= -1.95f)
+        {
+            transform.position = new Vector3(transform.position.x, -1.95f, 0f);
+        }
+
+        if (transform.position.x >= 11.27f)
+        {
+            transform.position = new Vector3(-11.27f, transform.position.y, 0f);
+        }
+        else if (transform.position.x <= -11.27f)
+        {
+            transform.position = new Vector3(11.27f, transform.position.y, 0f);
+        }
+    }
+    
+}
