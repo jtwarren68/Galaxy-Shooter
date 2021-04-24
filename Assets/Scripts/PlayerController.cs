84fd6743f6aa2e19864dff9a474d7f486f254b7e
jtwarren68 <81195744+jtwarren68@users.noreply.github.com>
2021-04-23 19:49:10 -0500

    Player movement code refactor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,42 +3,38 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    //private Transform _myTransform;
 
     void Start()
     {
-        // take the current position and assign start position to (o,0,0)
+        // On game start, set player position to (0,0,0)
         transform.position = Vector3.zero;
     }
 
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0f) * (_speed * Time.deltaTime));
-        
-        // if y = 0 stop movement
-        // if x = 11.7 then x = -11.7 and vice versa
 
-        if (transform.position.y >= 3f)
-        {
-            transform.position = new Vector3(transform.position.x, 3f, 0f);
-        }
-
-        if (transform.position.y <= -1.95f)
-        {
-            transform.position = new Vector3(transform.position.x, -1.95f, 0f);
-        }
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        
+        transform.Translate(direction * (_speed * Time.deltaTime));
+        
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,-4.1f, 1.5f), 0f);
 
         if (transform.position.x >= 11.27f)
         {
-            transform.position = new Vector3(-11.27f, transform.position.y, 0f);
+            transform.position = new Vector3(-11.27f, transform.position.y, 0);
         }
         else if (transform.position.x <= -11.27f)
         {
-            transform.position = new Vector3(11.27f, transform.position.y, 0f);
+            transform.position = new Vector3(11.27f, transform.position.y, 0);
         }
     }
     
 }
+
